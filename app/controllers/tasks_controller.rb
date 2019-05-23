93879a35aa60c59fe73d9249cc8c9505d95c45d7
Chad Carlson <chad.m.carlson@gmail.com>
2019-05-23 16:55:10 -0600

    lists showing in work order, task forms a work in progress

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,6 +6,12 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create_task(params.require(:task).permit(:task_title, :task_description, :list_id))   
+    redirect_to path 
   end
 
   def edit
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,16 @@
 class Task < ApplicationRecord
   belongs_to :list, optional: :true
+
+  #CLASS METHOD
+  def self.create_task(params)
+    Task.find_by_sql(["
+      INSERT INTO tasks (task_title, task_description, list_id, created_at, updated_at)
+      VALUES (:task_title, :task_description, :list_id, :created_at, :updated_at)", {
+        task_title: params[:task_title],
+        task_description: params[:task_description],
+        list_id: params[:list_id],
+        created_at: DateTime.now,
+        updated_at: DateTime.now
+      }])
+  end
 end
