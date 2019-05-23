0fff0d0ce1148268036b2ed67d3b40e69e411137
Chad Carlson <chad.m.carlson@gmail.com>
2019-05-23 15:57:54 -0600

    work order index and show pages working

--- app/controllers/work_orders_controller.rb
+++ app/controllers/work_orders_controller.rb
@@ -1,8 +1,13 @@
 class WorkOrdersController < ApplicationController
+
   def index
+    #WILL NEED TO ADD CURRENT_USER TO THIS IF DEVISE GETS ADDED
+    @work_orders = WorkOrder.all_workorders
   end
 
   def show
+    #@work_order = WorkOrder(params[:id])
+    @work_order = WorkOrder.find_workorder(params[:id])
   end
 
   def new
--- app/models/work_order.rb
+++ app/models/work_order.rb
@@ -2,4 +2,19 @@ class WorkOrder < ApplicationRecord
   
   has_many :lists, dependent: :destroy
 
+  #CLASS METHODS
+  def self.all_workorders
+    WorkOrder.find_by_sql("
+      SELECT *
+      FROM work_orders
+      ")
+  end
+
+  def self.find_workorder(params)
+    WorkOrder.find_by_sql("
+      SELECT *
+      FROM work_orders
+      WHERE work_orders.id = #{params}
+      ").first
+  end
 end
