11617fc28d8e59782e33b076b533d634d68e5965
Chad Carlson <chad.m.carlson@gmail.com>
2019-05-23 19:38:06 -0600

    lists showing tasks in work order show page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :set_list
+
   def index
   end
 
@@ -7,13 +9,20 @@ class TasksController < ApplicationController
 
   def new
     @task = Task.new
+    @list = List.find(params[:list_id])
   end
-
+  
   def create
+    @list = List.find(params[:list_id])
     @task = Task.create_task(params.require(:task).permit(:task_title, :task_description, :list_id))   
-    redirect_to path 
+    redirect_to work_order_path(@list.work_order_id)
   end
 
   def edit
+  end
+
+  private
+
+  def set_list
   end
 end
